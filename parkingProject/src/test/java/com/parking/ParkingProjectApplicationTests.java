abeb3f99726c4eb93d9c6301f68ea3fbf93279ff
antoniodi <antonio4793@hotmail.com>
2018-01-30 11:40:03 -0500

    Resolve prblem jpa

--- parkingProject/src/test/java/com/parking/ParkingProjectApplicationTests.java
+++ parkingProject/src/test/java/com/parking/ParkingProjectApplicationTests.java
@@ -1,5 +1,7 @@
 package com.parking;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +13,8 @@ public class ParkingProjectApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		
+		assertTrue(true);
 	}
 
 }
