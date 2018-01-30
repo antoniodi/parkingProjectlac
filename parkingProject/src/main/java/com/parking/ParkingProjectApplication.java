cfc625534e6aa9e8086bf3c0e8b4c2de7aef138e
antoniodi <antonio4793@hotmail.com>
2018-01-30 09:46:16 -0500

    projecto
    estructura inicial

---
+++ parkingProject/src/main/java/com/parking/ParkingProjectApplication.java
@@ -0,0 +1,12 @@
+package com.parking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingProjectApplication.class, args);
+	}
+}
