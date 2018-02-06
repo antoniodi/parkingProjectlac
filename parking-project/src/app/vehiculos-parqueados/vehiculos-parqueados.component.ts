a24442f4cef72472bccd6a9f990f27e9462231e4
antoniodi <antonio4793@hotmail.com>
2018-02-06 17:01:26 -0500

    Arreglar la tabla

--- parking-project/src/app/vehiculos-parqueados/vehiculos-parqueados.component.ts
+++ parking-project/src/app/vehiculos-parqueados/vehiculos-parqueados.component.ts
@@ -1,8 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { Http } from '@angular/http';
-import { Response } from '@angular/http/src/static_response';
-import { Observable } from 'rxjs/Observable';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ServicesParking } from '../services-parking';
+import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
 
 @Component({
   selector: 'app-vehiculos-parqueados',
@@ -13,7 +11,12 @@ import { ServicesParking } from '../services-parking';
 
 export class VehiculosParqueadosComponent implements OnInit {
 
+  displayedColumns = ['tipo', 'placa', 'cicindraje', 'fecha de ingreso'];
+  dataSource: MatTableDataSource<any>;
   myData: Array<any>;
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
   constructor(private servicesParking: ServicesParking) {
     this.getVehiculosParqueados();
@@ -27,7 +30,17 @@ export class VehiculosParqueadosComponent implements OnInit {
       );
   }
 
+  /**
+   * Set the paginator and sort after the view init since this component will
+   * be able to query its view for the initialized paginator and sort.
+   */
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
   ngOnInit() {
+    dataSource = new MatTableDataSource(users);
   }
 
 }
