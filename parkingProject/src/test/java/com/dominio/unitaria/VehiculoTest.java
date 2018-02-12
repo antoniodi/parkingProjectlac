516cae940f6df2c3fcb6da20978e71dc509d81f7
antoniodi <antonio4793@hotmail.com>
2018-02-12 16:01:47 -0500

    unitarias para vehiculo

--- parkingProject/src/test/java/com/dominio/unitaria/VehiculoTest.java
+++ parkingProject/src/test/java/com/dominio/unitaria/VehiculoTest.java
@@ -5,6 +5,7 @@ package com.dominio.unitaria;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import com.dominio.TipoDeVehiculo;
@@ -22,7 +23,7 @@ public class VehiculoTest {
 	private final int CILINDRAJE = 100;
 
 	@Test
-	public void test() {
+	public void construirVehiculo() {
 		//Arrange
 		VehiculoTestDataBuilder vehiculoTestDataBuilder = new VehiculoTestDataBuilder().
 				conCilindraje(CILINDRAJE).
@@ -32,10 +33,50 @@ public class VehiculoTest {
 		//Act 
 		Vehiculo vehiculo = vehiculoTestDataBuilder.buildVehiculo();
 		
-		//Arrange
+		//Assert
 		assertEquals(CILINDRAJE, vehiculo.getCilindraje());
 		assertEquals(PLACA, vehiculo.getPlaca());
 		assertEquals(TIPODEVEHICULO, vehiculo.getTipoDeVehiculo());		
+	}
+	
+	@Test
+	public void modificarPlaca() {
+		// Arrange
+		Vehiculo vehiculo = new Vehiculo();
+		String placa = "LIV-77D";
+		
+		// Act
+		vehiculo.setPlaca(placa);
+		
+		// Assert
+		Assert.assertEquals(placa, vehiculo.getPlaca());
+		
+	}
+	
+	@Test
+	public void modificarTipoDeVehiculo() {
+		// Arrange
+		Vehiculo vehiculo = new Vehiculo();
+		TipoDeVehiculo tipoDeVehiculo = TipoDeVehiculo.CARRO;
+		
+		// Act
+		vehiculo.setTipoDeVehiculo(tipoDeVehiculo);
+		
+		// Assert
+		Assert.assertEquals(tipoDeVehiculo, vehiculo.getTipoDeVehiculo());
+	}
+	
+	@Test
+	public void modificarCilindraje() {
+		// Arrange
+		Vehiculo vehiculo = new Vehiculo();
+		int cilindraje = 150;
+		
+		// Act
+		vehiculo.setCilindraje(cilindraje);
+		
+		// Assert
+		Assert.assertEquals(cilindraje, vehiculo.getCilindraje());
 	}
 
 }
